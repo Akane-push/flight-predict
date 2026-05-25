1efd0fab0da7346c1c35fc52be26cfad1210a5c0
Akane <edr.potdevin@gmail.com>
2026-05-25 15:55:58 +0000

    [Feat] Add skeleton classes for upcoming api modules

--- src/tools/XGBoost_GridSearch.py
+++ src/tools/XGBoost_GridSearch.py
@@ -17,8 +17,14 @@ class XGBGridSearch():
 
         df_flights = self.pl.read_parquet(f"{datas_path}/*{filename_flight}")
         df_weather = self.pl.read_parquet(f"{datas_path}/*{filename_weather}")
+
+        if self.df_flights.is_empty():
+            raise ValueError("[WARNING] No available flight datas")
+        
+        elif self.df_weather.is_empty():
+            raise ValueError("[WARNING] No available weather datas")
     
-        df_clean = cl(df_flights, df_weather).get_dataframe()
+        df_clean = cl().get_cleaned(df_flights, df_weather)
 
         feats = df_clean.drop("Delay")
         target = df_clean["Delay"]
