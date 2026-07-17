19d9a77e5d5da95375d9ce3898bcb572e715173b
Akane <edr.potdevin@gmail.com>
2026-07-17 08:56:32 +0000

    [Add] Add Prometheus, Grafana and StatsD monitoring (Docker Compose)

--- api-predict/main.py
+++ api-predict/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException, Request,  Depends
+from fastapi import FastAPI, Request, Depends
+from prometheus_fastapi_instrumentator import Instrumentator
+from importlib import resources
+import polars as pl
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from src.tools.api_key_auth import verify_api_key
 from src.tools.api_rate_limiter import limiter, rate_limit_exceeded_handler
 import json
-from importlib import resources
-import polars as pl
 
 # API init   ================================================
 app = FastAPI()
@@ -14,14 +15,19 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
+Instrumentator().instrument(app).expose(app)
+
 iata_path = resources.files("src.reference_data").joinpath("study_airport.json")
 
 with iata_path.open("r", encoding="utf-8") as f:
     iata_list = json.load(f)
 
-df_airports = pl.read_parquet(resources.files("src.reference_data").joinpath("airports_references.parquet"))
+df_airports = pl.read_parquet(
+    resources.files("src.reference_data").joinpath("airports_references.parquet")
+)
 
 airports_list = df_airports.filter(pl.col("Airport_IATA").is_in(iata_list))
+
 
 @app.get("/available_airports")
 def get_items(request: Request, api_key: str = Depends(verify_api_key)):
--- api-prod/main.py
+++ api-prod/main.py
@@ -2,6 +2,7 @@ import os
 import httpx
 import datetime
 from fastapi import FastAPI, HTTPException, Request, status, Depends
+from prometheus_fastapi_instrumentator import Instrumentator
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from src.tools.api_rate_limiter import limiter, rate_limit_exceeded_handler
@@ -17,16 +18,18 @@ dag_id_weather = "ad_hoc_weather"
 # API init   ================================================
 http_client: httpx.AsyncClient = None
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global http_client
     http_client = httpx.AsyncClient(
         base_url=airflow_url,
         timeout=5.0,
-        limits=httpx.Limits(max_keepalive_connections=4, max_connections=5)
+        limits=httpx.Limits(max_keepalive_connections=4, max_connections=5),
     )
     yield
     await http_client.aclose()
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -34,45 +37,57 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
+Instrumentator().instrument(app).expose(app)
+
 # Connexion with Airflow API token   ================================================
 security = HTTPBearer()
 
-async def verify_airflow_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
+
+async def verify_airflow_token(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+) -> dict:
     token = credentials.credentials
-    
+
     try:
         response = await http_client.get(
-            "/api/v2/dags?limit=1", 
-            headers={"Authorization": f"Bearer {token}"}
+            "/api/v2/dags?limit=1", headers={"Authorization": f"Bearer {token}"}
         )
-        
+
         if response.status_code == status.HTTP_401_UNAUTHORIZED:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="[WARNING] Invalid or expired Airflow token"
+                detail="[WARNING] Invalid or expired Airflow token",
             )
 
         elif response.status_code == status.HTTP_403_FORBIDDEN:
             return token
-            
+
         elif response.status_code != status.HTTP_200_OK:
             raise HTTPException(
                 status_code=status.HTTP_502_BAD_GATEWAY,
-                detail=f"[WARNING] Airflow Auth Error [Status {response.status_code}]: {response.text}"
+                detail=f"[WARNING] Airflow Auth Error [Status {response.status_code}]: {response.text}",
             )
-            
+
         return token
-        
+
     except httpx.RequestError as exc:
         raise HTTPException(
             status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-            detail=f"[WARNING] Failed to connect to Airflow: {exc}"
+            detail=f"[WARNING] Failed to connect to Airflow: {exc}",
         )
 
+
 # Trigger flight dag  ================================================
-@app.post("/trigger-ad_hoc-flight/{date_val}/{time_val}", status_code=status.HTTP_201_CREATED)
+@app.post(
+    "/trigger-ad_hoc-flight/{date_val}/{time_val}", status_code=status.HTTP_201_CREATED
+)
 @limiter.limit("10/hour")
-async def trigger_dag_flight(date_val: str, time_val: str, c_token: Annotated[str, Depends(verify_airflow_token)], request: Request):
+async def trigger_dag_flight(
+    date_val: str,
+    time_val: str,
+    c_token: Annotated[str, Depends(verify_airflow_token)],
+    request: Request,
+):
     """Triggers the DAG
     Date format YYYY-MM-DD
     Time format HH:mm
@@ -80,19 +95,20 @@ async def trigger_dag_flight(date_val: str, time_val: str, c_token: Annotated[st
     validate_date_format(date_val)
     validate_time_format(time_val)
 
-    now_utc = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    now_utc = datetime.datetime.now(datetime.timezone.utc).strftime(
+        "%Y-%m-%dT%H:%M:%SZ"
+    )
     url = f"/api/v2/dags/{dag_id_flight}/dagRuns"
-    
+
     dag_run_data = {
-            "logical_date": now_utc,
-            "conf": {
-                "date_val": date_val,
-                "time_val": time_val
-            }
-        }
+        "logical_date": now_utc,
+        "conf": {"date_val": date_val, "time_val": time_val},
+    }
 
     try:
-        response = await http_client.post(url, json=dag_run_data, headers={"Authorization": f"Bearer {c_token}"})
+        response = await http_client.post(
+            url, json=dag_run_data, headers={"Authorization": f"Bearer {c_token}"}
+        )
 
         if response.status_code not in [200, 201]:
             raise HTTPException(
@@ -108,27 +124,31 @@ async def trigger_dag_flight(date_val: str, time_val: str, c_token: Annotated[st
             detail=f"[WARNING] Failed to connect to Airflow: {exc}",
         )
 
+
 # Trigger weather dag  ================================================
 @app.post("/trigger-ad_hoc-weather/{date_val}", status_code=status.HTTP_201_CREATED)
 @limiter.limit("10/hour")
-async def trigger_dag_weather(date_val: str, c_token: Annotated[str, Depends(verify_airflow_token)], request: Request):
+async def trigger_dag_weather(
+    date_val: str,
+    c_token: Annotated[str, Depends(verify_airflow_token)],
+    request: Request,
+):
     """Triggers the DAG
     Date format YYYY-MM-DD
     """
     validate_date_format(date_val)
 
-    now_utc = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    now_utc = datetime.datetime.now(datetime.timezone.utc).strftime(
+        "%Y-%m-%dT%H:%M:%SZ"
+    )
     url = f"/api/v2/dags/{dag_id_weather}/dagRuns"
-    
-    dag_run_data = {
-            "logical_date": now_utc,
-            "conf": {
-                "date_val": date_val
-            }
-        }
+
+    dag_run_data = {"logical_date": now_utc, "conf": {"date_val": date_val}}
 
     try:
-        response = await http_client.post(url, json=dag_run_data, headers={"Authorization": f"Bearer {c_token}"})
+        response = await http_client.post(
+            url, json=dag_run_data, headers={"Authorization": f"Bearer {c_token}"}
+        )
 
         if response.status_code not in [200, 201]:
             raise HTTPException(
